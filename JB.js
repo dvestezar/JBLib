31f16a8a6c3ade920c6188ee2e66a02bb64b5399
dvestezar <info@dvestezar.cz>
2014-05-02 16:44:19 +0200

    v 2.0.4.19

--- JB.js
+++ JB.js
@@ -3,8 +3,9 @@ JB tools
 (c)2008
 www.dvetezar.cz
 
-v 2.0.4.10
+v 2.0.4.19
 */
+var lns_month = [31,28,31,30,31,30,31,31,30,31,30,31];
 
 if(typeof JB == 'undefined'){
 	var JB = new Object;
@@ -18,6 +19,29 @@ String.prototype.trim=function(){
 	//provede trim
 	var a=String(this.valueOf());
 	return a.replace(/(^\s+)|(\s+$)/gi,'');
+}
+
+//pokud neexistuje bind tak vytvoř
+//viz https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function (oThis) {
+		if (typeof this !== "function") {
+			// closest thing possible to the ECMAScript 5 internal IsCallable function
+			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+		}
+		var aArgs = Array.prototype.slice.call(arguments, 1), 
+			fToBind = this, 
+			fNOP = function () {},
+			fBound = function () {
+			return fToBind.apply(this instanceof fNOP && oThis
+				? this
+				: oThis,
+				aArgs.concat(Array.prototype.slice.call(arguments)));
+			};
+		fNOP.prototype = this.prototype;
+		fBound.prototype = new fNOP();
+		return fBound;
+	};
 }
 
 function JBconvToUniEsc(a,rem){
@@ -38,6 +62,8 @@ function JBconvToUniEsc(a,rem){
 
 if(JB.x==undefined){
 	JB.x=new function(){
+		var ent_uni={'quot':34,'amp':38,'apos':39,'lt':60,'gt':62,'nbsp':160,'iexcl':161,'cent':162,'pound':163,'curren':164,'yen':165,'brvbar':166,'sect':167,'uml':168,'copy':169,'ordf':170,'laquo':171,'not':172,'shy':173,'reg':174,'macr':175,'deg':176,'plusmn':177,'sup2':178,'sup3':179,'acute':180,'micro':181,'para':182,'middot':183,'cedil':184,'sup1':185,'ordm':186,'raquo':187,'frac14':188,'frac12':189,'frac34':190,'iquest':191,'Agrave':192,'Aacute':193,'Acirc':194,'Atilde':195,'Auml':196,'Aring':197,'AElig':198,'Ccedil':199,'Egrave':200,'Eacute':201,'Ecirc':202,'Euml':203,'Igrave':204,'Iacute':205,'Icirc':206,'Iuml':207,'ETH':208,'Ntilde':209,'Ograve':210,'Oacute':211,'Ocirc':212,'Otilde':213,'Ouml':214,'times':215,'Oslash':216,'Ugrave':217,'Uacute':218,'Ucirc':219,'Uuml':220,'Yacute':221,'THORN':222,'szlig':223,'agrave':224,'aacute':225,'acirc':226,'atilde':227,'auml':228,'aring':229,'aelig':230,'ccedil':231,'egrave':232,'eacute':233,'ecirc':234,'euml':235,'igrave':236,'iacute':237,'icirc':238,'iuml':239,'eth':240,'ntilde':241,'ograve':242,'oacute':243,'ocirc':244,'otilde':245,'ouml':246,'divide':247,'oslash':248,'ugrave':249,'uacute':250,'ucirc':251,'uuml':252,'yacute':253,'thorn':254,'yuml':255,'OElig':338,'oelig':339,'Scaron':352,'scaron':353,'Yuml':376,'fnof':402,'circ':710,'tilde':732,'Alpha':913,'Beta':914,'Gamma':915,'Delta':916,'Epsilon':917,'Zeta':918,'Eta':919,'Theta':920,'Iota':921,'Kappa':922,'Lambda':923,'Mu':924,'Nu':925,'Xi':926,'Omicron':927,'Pi':928,'Rho':929,'Sigma':931,'Tau':932,'Upsilon':933,'Phi':934,'Chi':935,'Psi':936,'Omega':937,'alpha':945,'beta':946,'gamma':947,'delta':948,'epsilon':949,'zeta':950,'eta':951,'theta':952,'iota':953,'kappa':954,'lambda':955,'mu':956,'nu':957,'xi':958,'omicron':959,'pi':960,'rho':961,'sigmaf':962,'sigma':963,'tau':964,'upsilon':965,'phi':966,'chi':967,'psi':968,'omega':969,'thetasym':977,'upsih':978,'piv':982,'ensp':8194,'emsp':8195,'thinsp':8201,'zwnj':8204,'zwj':8205,'lrm':8206,'rlm':8207,'ndash':8211,'mdash':8212,'lsquo':8216,'rsquo':8217,'sbquo':8218,'ldquo':8220,'rdquo':8221,'bdquo':8222,'dagger':8224,'Dagger':8225,'bull':8226,'hellip':8230,'permil':8240,'prime':8242,'Prime':8243,'lsaquo':8249,'rsaquo':8250,'oline':8254,'frasl':8260,'euro':8364,'image':8465,'weierp':8472,'real':8476,'trade':8482,'alefsym':8501,'larr':8592,'uarr':8593,'rarr':8594,'darr':8595,'harr':8596,'crarr':8629,'lArr':8656,'uArr':8657,'rArr':8658,'dArr':8659,'hArr':8660,'forall':8704,'part':8706,'exist':8707,'empty':8709,'nabla':8711,'isin':8712,'notin':8713,'ni':8715,'prod':8719,'sum':8721,'minus':8722,'lowast':8727,'radic':8730,'prop':8733,'infin':8734,'ang':8736,'and':8743,'or':8744,'cap':8745,'cup':8746,'int':8747,'there4':8756,'sim':8764,'cong':8773,'asymp':8776,'ne':8800,'equiv':8801,'le':8804,'ge':8805,'sub':8834,'sup':8835,'nsub':8836,'sube':8838,'supe':8839,'oplus':8853,'otimes':8855,'perp':8869,'sdot':8901,'vellip':8942,'lceil':8968,'rceil':8969,'lfloor':8970,'rfloor':8971,'lang':9001,'rang':9002,'loz':9674,'spades':9824,'clubs':9827,'hearts':9829,'diams':9830};
+	
 		this.BR = function(p){
 			/* vytvoří HTML element <br> s parametry v p
 				vrací pole s elementy br
@@ -309,7 +335,7 @@ if(JB.x==undefined){
 		this.byte_to_text = function(co,zaok){
 		// opraví číslo v "co" na textové vyjádření kB,MB,GB,TB a zaokrouhlí na "zaok" desetin
 		var x,a
-		var conv_byte_tx = ["","kB","MB","GB","TB"];
+		var conv_byte_tx = ["B","kB","MB","GB","TB"];
 			x=0
 			while ((co>1024)&&(x<4)){
 				co=co/1024;
@@ -367,6 +393,37 @@ if(JB.x==undefined){
 			}
 			return a;
 		}
+		this.removeHTMLentities =function(tx,def){
+			/*odstraní html entities
+			 def (default = true) pokud nastavíme na false, tak je převedeno vše, jinak je v základu ponecháno	&amp; &lt; &gt; &quot;
+			*/
+			if(JB.is.und(def))
+				def=true;
+			return String(tx).replace(/&#(\d+);?/g, function (_, code) {
+				return String.fromCharCode(code);
+			})
+			.replace(/&#[xX]([A-Fa-f0-9]+);?/g, function (_, hex) {
+				return String.fromCharCode(parseInt(hex, 16));
+			})
+			.replace(/&([^;\W]+;?)/g, function (m, e) {
+				var ee = e.replace(/;$/, '');
+				if( !(/((amp)|(lt)|(gt)|(quot))/.test(ee) && def) || !def){
+					var target = ent_uni[e] || (e.match(/;$/) && ent_uni[ee]);
+						
+					if (typeof target === 'number') {
+					  return String.fromCharCode(target);
+					}
+					else if (typeof target === 'string') {
+					  return target;
+					}
+					else {
+					  return m;
+					}
+				}else{
+					return m;
+				}
+			})
+		}			
 	}
 }
 
@@ -1699,10 +1756,10 @@ JB.date = new function(){
 	//	  = "y"=roky
 	// co není case
 	// kolik záporná, nebo kladná hodnota
-		if(IsNumber(kolik)==false){return 0};
+		if(JB.is.number(kolik)==false){return 0};
 		if(kolik!=Math.floor(kolik)){return 0};
 		kolik=Math.floor(kolik);
-		if(IsNumber(dttm)==false){return 0};
+		if(JB.is.number(dttm)==false){return 0};
 	var a,b;
 	var h=1/24;
 	var m=h/60;
@@ -1772,7 +1829,7 @@ JB.date = new function(){
 	this.toNumDate = function(x){
 	//převede datum na číslo
 	var a,b,y,m,d,c
-		if (IsDatum(x)==false){return 0};
+		if (JB.is.date(x)==false){return 0};
 		x=x.split(".");
 		a=x[2]-1900;
 		m=x[1]-1;
@@ -1789,7 +1846,7 @@ JB.date = new function(){
 	this.NumToDate = function(x){
 	//převede číslo na datum
 	var a,y
-		if(IsNumber(x)==false){return ""};
+		if(JB.is.number(x)==false){return ""};
 		a=Math.floor(x/1461);//1461= 4*365+1
 		x-=a*1461;
 		y=a*4;		
@@ -1804,7 +1861,7 @@ JB.date = new function(){
 		return ((x+1)+"."+(a+1)+"."+y);	
 	}
 	this.TimeToNum=function(x){
-	if (IsCas(x)==false){return 0};
+	if (JB.is.time(x)==false){return 0};
 	var h=1/24;
 	var m=h/60;
 	var s=m/60;
@@ -1818,7 +1875,7 @@ JB.date = new function(){
 	this.NumToTime=function(x){
 	var a=24*60*60;
 	var h,m;
-		if(IsNumber(x)==false){return ""};
+		if(JB.is.number(x)==false){return ""};
 
 		if ((x>=1)||(x<=0)){return 0};
 		x=Math.round(x*a);
@@ -1834,7 +1891,7 @@ JB.date = new function(){
 
 	this.NumToDatetime=function(x){
 	var a;
-		if(IsNumber(x)==false){return ""};
+		if(JB.is.number(x)==false){return ""};
 		a=Math.floor(x);
 		x-=a;
 		return (this.NumToDate(a)+" "+this.NumToTime(x));
@@ -1866,7 +1923,7 @@ JB.date = new function(){
 		if(typeof typ == 'undefined'){
 			typ=0;
 		}
-		if(IsNumber(x)==false){return ""};
+		if(JB.is.number(x)==false){return ""};
 		var a=Math.floor(x);
 		x-=a;
 		switch (typ){
@@ -1883,6 +1940,11 @@ JB.date = new function(){
 				return this.NumToDate(a)+(x==0?' 0:0:0':' '+this.NumToTime(x));
 				break;
 		}
+	}
+	
+	this.now=function(){
+		var a=new Date();
+		return this.correct(a.getFullYear()+'-'+(a.getMonth()+1)+'-'+a.getDate()+' '+a.getHours()+':'+a.getMinutes()+':'+a.getSeconds());
 	}
 }
 JB.html = new function(){
